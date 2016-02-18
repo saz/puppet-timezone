8339aed7bbdb25f9dfa72395d3f001908141d4f0
Shinya Tsunematsu <s@tnmt.info>
2016-02-19 02:43:40 +0900

    update command doesn't depend on timezone_file
    update command is called when timezone_file is changed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -103,18 +103,19 @@ class timezone (
     file { $timezone::params::timezone_file:
       ensure  => $timezone_ensure,
       content => template($timezone::params::timezone_file_template),
+      notify  => Exec['update_timezone'],
     }
-    if $ensure == 'present' and $timezone::params::timezone_update {
-      $e_command = $timezone::params::timezone_update_arg ? {
-        true  => "${timezone::params::timezone_update} ${timezone}",
-        false => $timezone::params::timezone_update
-      }
-      exec { 'update_timezone':
-        command     => $e_command,
-        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-        subscribe   => File[$timezone::params::timezone_file],
-        refreshonly => true,
-      }
+  }
+
+  if $ensure == 'present' and $timezone::params::timezone_update {
+    $e_command = $timezone::params::timezone_update_arg ? {
+      true  => "${timezone::params::timezone_update} ${timezone}",
+      false => $timezone::params::timezone_update
+    }
+    exec { 'update_timezone':
+      command     => $e_command,
+      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+      refreshonly => true,
     }
   }
 
