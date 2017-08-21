871cbd97222c58e1856bb3f70cddbcf7eaa6cc49
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-08-21 17:11:54 -0400

    Migration to Data in Modules
    This mainly just gets rid of params.pp and maps everything down into
    module data.
    
    The lower release support for Puppet is set at 4.7.0 since this is the
    earliest version currently supported by the vendor in the PE releases.
    
    Closes #47

--- spec/classes/timezone_spec.rb
+++ spec/classes/timezone_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe 'timezone' do
   %w[Debian RedHat Gentoo FreeBSD].each do |osfamily|
     describe "on supported osfamily: #{osfamily}" do
+      let(:title) { 'EST' }
+
       include_examples osfamily
     end
   end
--- spec/support/gentoo.rb
+++ spec/support/gentoo.rb
@@ -1,40 +1,49 @@
 shared_examples 'Gentoo' do
-  let(:facts) { { osfamily: 'Gentoo' } }
+  let(:facts) do
+    {
+      :os => {
+        :family => 'Gentoo',
+        :release => {
+          :full => '4.8.0',
+          :major => '4'
+        }
+      }
+    }
+  end
 
   describe 'when using default class parameters' do
     let(:params) { {} }
 
     it { is_expected.to create_class('timezone') }
-    it { is_expected.to contain_class('timezone::params') }
 
     it do
-      is_expected.to contain_package('sys-libs/timezone-data').with(ensure: 'present',
-                                                                    before: 'File[/etc/localtime]')
+      is_expected.to contain_package('sys-libs/timezone-data').with(:ensure => 'present',
+                                                                    :before => 'File[/etc/localtime]')
     end
 
     it { is_expected.to contain_file('/etc/timezone').with_ensure('file') }
-    it { is_expected.to contain_file('/etc/timezone').with_content(%r{^Etc\/UTC$}) }
+    it { is_expected.to contain_file('/etc/timezone').with_content(%r{^Etc/UTC$}) }
     it { is_expected.to contain_exec('update_timezone').with_command(%r{^emerge --config timezone-data$}) }
     it do
-      is_expected.to contain_file('/etc/localtime').with(ensure: 'file',
-                                                         source: 'file:///usr/share/zoneinfo/Etc/UTC')
+      is_expected.to contain_file('/etc/localtime').with(:ensure => 'file',
+                                                         :source => 'file:///usr/share/zoneinfo/Etc/UTC')
     end
 
     context 'when timezone => "Europe/Berlin"' do
-      let(:params) { { timezone: 'Europe/Berlin' } }
+      let(:params) { { :timezone => 'Europe/Berlin' } }
 
-      it { is_expected.to contain_file('/etc/timezone').with_content(%r{^Europe\/Berlin$}) }
+      it { is_expected.to contain_file('/etc/timezone').with_content(%r{^Europe/Berlin$}) }
       it { is_expected.to contain_file('/etc/localtime').with_source('file:///usr/share/zoneinfo/Europe/Berlin') }
     end
 
     context 'when autoupgrade => true' do
-      let(:params) { { autoupgrade: true } }
+      let(:params) { { :autoupgrade => true } }
 
       it { is_expected.to contain_package('sys-libs/timezone-data').with_ensure('latest') }
     end
 
     context 'when ensure => absent' do
-      let(:params) { { ensure: 'absent' } }
+      let(:params) { { :ensure => 'absent' } }
 
       it { is_expected.to contain_package('sys-libs/timezone-data').with_ensure('present') }
       it { is_expected.to contain_file('/etc/timezone').with_ensure('absent') }
